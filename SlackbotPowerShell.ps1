f1df84a571785f1b361c720cad811e89717ea077
Robert Rozas Navarro <robert.rozas.n@outlook.com>
2018-05-14 11:30:07 -0300

    Se añade script

---
+++ SlackbotPowerShell.ps1
@@ -0,0 +1,7 @@
+﻿
+
+        $body = [PSCustomObject]@{"text"="Mensaje a todo el equipo, release completada! "} | ConvertTo-Json
+        $Uri = "https://hooks.slack.com/services/xxxxxxxxx/yyyyyyyyyyyyyyyyyyyyy"
+        
+		Invoke-WebRequest -Uri $Uri -Method Post -ContentType "application/json;charset=utf-8" -Body $body 						
+		
